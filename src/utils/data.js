8b1838214099f4c30f60af8142c7a34fbe751d52
jeel1802 <160886657+jeel1802@users.noreply.github.com>
2024-11-16 11:39:53 +0530

    commit

--- src/utils/data.js
+++ src/utils/data.js
@@ -3,28 +3,28 @@ export const SkillsData = [
         title : "Frontend",
         icon : 'src/assets/img/front-end.png',
         skills : [
-            {skill : "HTML5" , percentage : "92%"},
-            {skill : "CSS" , percentage : '90%'},
+            {skill : "HTML5" , percentage : "98%"},
+            {skill : "CSS" , percentage : '92%'},
             {skill : "JavaScript" , percentage : '80%'},
-            {skill : "React.js" , percentage : '70%'},
+            {skill : "React.js" , percentage : '75%'},
         ],
     },
     {
         title : "Backend",
         icon : 'src/assets/img/back-end.png',
         skills : [
-            {skill : "Node.js" , percentage : "92%"},
-            {skill : "Express.js" , percentage : '90%'},
+            {skill : "Node.js" , percentage : "50%"},
+            {skill : "Express.js" , percentage : '45%'},
         ],
     },
     {
         title : "Tools",
         icon : 'src/assets/img/tools.png',
         skills : [
-            {skill : "HTML5" , percentage : "92%"},
-            {skill : "CSS" , percentage : '90%'},
-            {skill : "JavaScript" , percentage : '80%'},
-            {skill : "React.js" , percentage : '70%'},
+            {skill : "Git / Github" , percentage : "92%"},
+            {skill : "Visual studio code" , percentage : '98%'},
+            {skill : "Figma" , percentage : '70%'},
+            {skill : "Postman" , percentage : '85%'},
         ],
     },
     {
