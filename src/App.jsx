75a3f892fa937f9cee2e14cb369ea3bbd33db6c5
jeel1802 <160886657+jeel1802@users.noreply.github.com>
2025-06-04 15:31:13 +0530

    push

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Hero from "./sections/Hero";
+import  NavbarDemo  from "./sections/Navbar";
+
+function App() {
+  return (
+    <>
+      <div className="container mx-auto max-w-7xl">
+        <NavbarDemo/>
+        <Hero/>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/sections/Navbar.jsx
@@ -0,0 +1,92 @@
+"use client";
+import {
+  Navbar,
+  NavBody,
+  NavItems,
+  MobileNav,
+  NavbarLogo,
+  NavbarButton,
+  MobileNavHeader,
+  MobileNavToggle,
+  MobileNavMenu,
+} from "../components/resizable-navbar";
+import { useState } from "react";
+
+export default function NavbarDemo() {
+  const navItems = [
+    {
+      name: "Home",
+      link: "#home",
+    },
+    {
+      name: "About",
+      link: "#about",
+    },
+    {
+      name: "Work",
+      link: "#work",
+    },
+    // {
+    //     name : "Contact",
+    //     link : "#contact"
+    // }
+  ];
+
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  return (
+    <div className="relative w-full">
+      <Navbar>
+        {/* Desktop Navigation */}
+        <NavBody>
+          <NavbarLogo />
+          <NavItems items={navItems} />
+          <div className="flex items-center gap-4">
+            {/* <NavbarButton variant="secondary">Login</NavbarButton> */}
+            <NavbarButton variant="primary">
+                <a href="#contact">Contact Me</a>
+            </NavbarButton>
+          </div>
+        </NavBody>
+
+        {/* Mobile Navigation */}
+        <MobileNav>
+          <MobileNavHeader>
+            <NavbarLogo />
+            <MobileNavToggle
+              isOpen={isMobileMenuOpen}
+              onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)} />
+          </MobileNavHeader>
+
+          <MobileNavMenu isOpen={isMobileMenuOpen} onClose={() => setIsMobileMenuOpen(false)}>
+            {navItems.map((item, idx) => (
+              <a
+                key={`mobile-link-${idx}`}
+                href={item.link}
+                onClick={() => setIsMobileMenuOpen(false)}
+                className="relative text-neutral-600 dark:text-neutral-300">
+                <span className="block">{item.name}</span>
+              </a>
+            ))}
+            <div className="flex w-full flex-col gap-4">
+              {/* <NavbarButton
+                onClick={() => setIsMobileMenuOpen(false)}
+                variant="primary"
+                className="w-full">
+                Login
+              </NavbarButton> */}
+              <NavbarButton
+                onClick={() => setIsMobileMenuOpen(false)}
+                variant="primary"
+                className="w-full">
+                <a href="#contact">Contact Me</a>
+              </NavbarButton>
+            </div>
+          </MobileNavMenu>
+        </MobileNav>
+      </Navbar>
+      {/* Navbar */}
+    </div>
+  );
+}
+
